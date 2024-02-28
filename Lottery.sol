ccf2c388ff855c5fa67648995268d2ee97f22317
creative enough <121463341+sirALp@users.noreply.github.com>
2024-02-28 13:37:38 +0300

    CommentLines Added.sol

--- Lottery.sol
+++ Lottery.sol
@@ -16,22 +16,24 @@ contract Lottery{
     address payable []  public players;
     uint256 public lottery_id;
     mapping(uint256 => address) lottery_history;
-
+    
     function listPlayers() public view returns(address[] memory _players){
         return _players;
     }
-
+    
     constructor(){
         owner = msg.sender;
         lottery_id = 1;
     }
-
+    
     function enterLottery() public payable{
         require(msg.value >= .001 ether,"Insufficent ether");
         //payable(msg.sender).transfer(address(this).balance);
         players.push (payable(msg.sender));
     }
 
+
+    // !!!not a convenient way to generate random number considering more advanced applications 
     function getRandomNumber()internal view returns(uint256){
         return uint256(keccak256(abi.encode(owner,block.timestamp,players.length)));
     }
